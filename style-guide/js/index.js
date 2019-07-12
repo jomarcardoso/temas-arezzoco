692bcd34738f9ca240f5ef320c7e60307e4b2942
jomar.cardoso <Jo5592720>
2019-07-11 21:31:19 -0300

    scroll to

--- style-guide/js/index.js
+++ style-guide/js/index.js
@@ -1,30 +1,44 @@
-import IndexDetector from './index-detector.js';
-
 document.addEventListener('DOMContentLoaded', () => {
   let iDocument = null;
+  let iBody = null;
 
   function addStyleToIframe() {
-    const iStyle = iDocument.createElement('style');
-    iStyle.innerHTML = `
-    .sg-Main-container {
-      margin: 0 auto;
-      max-width: 960px;
-      padding: 15px;
-    }`;
+    const iLink = iDocument.createElement('link');
+    iLink.href= './style-guide/css/main.css';
+    iLink.rel = 'stylesheet';
 
-    iDocument.querySelector('head').appendChild(iStyle);
+    iDocument.querySelector('head').appendChild(iLink);
     iDocument.querySelector('body').classList.add('sg-Main-container');
   }
 
   function createNavList() {
-    function templateNavItem({ tagName, innerHTML, id }) {
+    function scrollTo(scroll) {
+      iBody.scrollTop = scroll;
+    }
+
+    function bindScrollEvents() {
+      const anchors = document.querySelectorAll('a[href*="#"]');
+      anchors.forEach((anchor) => {
+        anchor.addEventListener('click', ({ currentTarget: { hash } }) => {
+          // console.dir(currentTarget)
+          const target = iDocument.querySelector(hash);
+          scrollTo(target.offsetTop)
+        });
+      })
+    }
+
+    function templateNavItem({ tagName, innerHTML, id, offsetTop }) {
       return `
-        <li class="sg-Nav-item${tagName}">
-          <a href="#${id || ''}">
+        <li class="sg-Nav-item sg-Nav-item--${tagName}">
+          <a href="#${id || ''}" data-scroll="${offsetTop || 0}">
             ${innerHTML}
           </a>
         </li>
       `;
+    }
+
+    function addClass(el) {
+      el.classList.add('sg-Heading', `sg-Heading--${el.tagName}`)
     }
 
     const listIndex = iDocument.querySelectorAll('h1, h2, h3, h4, h5, h6');
@@ -34,11 +48,16 @@ document.addEventListener('DOMContentLoaded', () => {
       return previousValue.concat(templateNavItem(currentvalue));
     }, '');
 
+    console.log(arrayIndex)
+    arrayIndex.forEach(addClass)
+
     elStyleGuideNav.innerHTML = navLayout;
+    bindScrollEvents();
   }
 
   elStyleGuideWrapper.addEventListener('load', () => {
     iDocument = elStyleGuideWrapper.contentDocument;
+    iBody = iDocument.querySelector('body');
     addStyleToIframe();
     createNavList();
   });
